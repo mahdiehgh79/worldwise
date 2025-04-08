9def399ba404f59fa2b1668892a9a24cbda510cf
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-04-08 15:26:36 +0330

    add NavLink

--- src/componenets/AppNav.jsx
+++ src/componenets/AppNav.jsx
@@ -1,6 +1,16 @@
 import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 const AppNav = () => {
-  return <nav className={styles.nav}>App Navigation</nav>;
+  return <nav className={styles.nav}>
+     <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+  </nav>;
 };
 export default AppNav;
