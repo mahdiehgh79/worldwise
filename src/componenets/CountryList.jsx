c4acd699a655283bc3f0090072438b9784e4bc73
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-04-09 14:14:30 +0330

    Add files via upload

---
+++ src/componenets/CountryList.jsx
@@ -0,0 +1,25 @@
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+const CountryList = ({ isLoading, cities }) => {
+  if (isLoading) return <Spinner />;
+
+  if (!cities) return <Message />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+};
+export default CountryList;
