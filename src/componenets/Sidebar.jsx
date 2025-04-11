fae2f55a9c8f6ec3ec6fb5b30d77e5af1e084c16
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-04-11 15:21:58 +0330

    Update Sidebar.jsx

--- src/componenets/Sidebar.jsx
+++ src/componenets/Sidebar.jsx
@@ -1,11 +1,11 @@
-import styles from "./sidebar.module.css";
+import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import { Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className={styles.Sidebar}>
+    <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
