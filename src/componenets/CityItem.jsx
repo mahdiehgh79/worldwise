66f93d0d1320916d560fe81ce641d9a1caacf47a
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-04-22 16:14:04 +0330

    Update CityItem.jsx

--- src/componenets/CityItem.jsx
+++ src/componenets/CityItem.jsx
@@ -1,4 +1,7 @@
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
+import { Link } from "react-router-dom";
+
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -9,9 +12,15 @@ const formatDate = (date) =>
 
 const CityItem = ({ city }) => {
   const { cityName, emoji, date, id, position  } = city;
+  const { currentCity } = useCities();
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+     <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
