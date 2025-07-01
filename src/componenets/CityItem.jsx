35d532ea592d5dd31d134018390ecb55127f18f3
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-07-01 21:15:45 +0330

    create handleClick function for deleteCity

--- src/componenets/CityItem.jsx
+++ src/componenets/CityItem.jsx
@@ -12,7 +12,12 @@ const formatDate = (date) =>
 
 const CityItem = ({ city }) => {
   const { cityName, emoji, date, id, position  } = city;
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
+  function handleClick(e) {
+    e.preventDefault();
+    // console.log("test");
+    deleteCity(id);
+  }
   return (
     <li>
      <Link
@@ -24,7 +29,7 @@ const CityItem = ({ city }) => {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
       </Link>
     </li>
   );
